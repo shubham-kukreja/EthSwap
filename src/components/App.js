29756701e30b95a32e2d54f6ac6a405430952651
skukreja <skukreja434@gmail.com>
2020-06-15 00:00:48 +0530

    Frontend-Setup

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,18 @@ import React, { Component } from "react";
 import "./App.css";
 import Web3 from "web3";
 import Navbar from "./Navbar";
+import EthSwap from "../abis/EthSwap.json";
+import Token from "../abis/Token.json";
+import Main from "./Main";
 
 class App extends Component {
   state = {
     account: "",
-    ethBalance: "",
+    ethBalance: "0",
+    token: {},
+    tokenBalance: "0",
+    ethSwap: {},
+    loading: true,
   };
 
   async loadWeb3() {
@@ -20,12 +27,35 @@ class App extends Component {
 
   async loadBlockchainData() {
     const web3 = window.web3;
+
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0] });
 
     const ethBalance = await web3.eth.getBalance(this.state.account);
     this.setState({ ethBalance });
-    console.log(this.state);
+
+    const networkId = await web3.eth.net.getId();
+    const tokenData = Token.networks[networkId];
+    if (tokenData) {
+      const token = new web3.eth.Contract(Token.abi, tokenData.address);
+      this.setState({ token });
+      let tokenBalance = await token.methods
+        .balanceOf(this.state.account)
+        .call();
+      this.setState({ tokenBalance: tokenBalance.toString() });
+    } else {
+      window.alert("Token Contract Not Deployed.");
+    }
+
+    const ethSwapData = EthSwap.networks[networkId];
+    if (ethSwapData) {
+      const ethSwap = new web3.eth.Contract(EthSwap.abi, ethSwapData.address);
+      this.setState({ ethSwap });
+    } else {
+      window.alert("Token Contract Not Deployed.");
+    }
+
+    this.setState({ loading: false });
   }
 
   async componentWillMount() {
@@ -39,9 +69,19 @@ class App extends Component {
         <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main
+              role="main"
+              className="col-lg-12 ml-auto mr-auto text-center"
+              style={{ maxWidth: "600px" }}
+            >
               <div className="content mr-auto ml-auto">
-                <h1>Dapp University Starter Kit</h1>
+                {this.state.loading ? (
+                  <div className="spinner-border text-primary" role="status">
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                ) : (
+                  <Main />
+                )}
               </div>
             </main>
           </div>
