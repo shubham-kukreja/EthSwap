75065bece4c4b37ad295b10ab438145c9801a4fb
skukreja <skukreja434@gmail.com>
2020-06-14 23:34:45 +0530

    NavBar-Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,42 @@
 import React, { Component } from "react";
 import "./App.css";
+import Web3 from "web3";
+import Navbar from "./Navbar";
 
 class App extends Component {
+  state = {
+    account: "",
+    ethBalance: "",
+  };
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else {
+      window.alert("Please Install MetaMask.");
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+
+    const ethBalance = await web3.eth.getBalance(this.state.account);
+    this.setState({ ethBalance });
+    console.log(this.state);
+  }
+
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
